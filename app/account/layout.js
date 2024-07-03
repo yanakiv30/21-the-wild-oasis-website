efae785f0f008370ec0d94bdcdae5c36057b741c
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-03 08:03:57 +0300

    account layout

---
+++ app/account/layout.js
@@ -0,0 +1,9 @@
+function layout() {
+    return (
+        <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
+           <div>Navigation</div> 
+           <div>Content</div>
+        </div>
+    )
+}
+export default layout
