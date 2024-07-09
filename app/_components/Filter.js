ed0d27a4e28dd0d6b5068a32288f6950c7bd0317
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-09 16:55:58 +0300

    Button

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -16,31 +16,51 @@ function Filter() {
 
   return (
     <div className="border border-primary-800 flex">
-      <button
-        onClick={() => handleFilter("all")}
-        className="px-5 py-2 hover:bg-primary-700"
+      
+      <Button
+        filter="all"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         All cabins
-      </button>
-      <button
-        onClick={() => handleFilter("small")}
-        className="px-5 py-2 hover:bg-primary-700"
+      </Button>
+      <Button
+        filter="small"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
-        1&mdash;3 guest
-      </button>
-      <button
-        onClick={() => handleFilter("medium")}
-        className="px-5 py-2 hover:bg-primary-700"
+        2&mdash;3 guest
+      </Button>
+
+      <Button
+        filter="medium"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         4&mdash;7 guest
-      </button>
-      <button
-        onClick={() => handleFilter("large")}
-        className="px-5 py-2 hover:bg-primary-700"
+      </Button>
+
+      <Button
+        filter="large"
+        handleFilter={handleFilter}
+        activeFilter={activeFilter}
       >
         8&mdash;12 guest
-      </button>
+      </Button>
     </div>
   );
+
+  function Button({ filter, handleFilter, activeFilter, children }) {
+    return (
+      <button
+        onClick={() => handleFilter(filter)}
+        className={`px-5 py-2 hover:bg-primary-700 ${
+          filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+        }`}
+      >
+        {children}
+      </button>
+    );
+  }
 }
 export default Filter;
