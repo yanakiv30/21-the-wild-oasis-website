04b7f3d7953891d41ed44f43dd077056f06db9a4
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-10 13:18:54 +0300

    Suspense

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,14 +1,13 @@
-import DateSelector from "@/app/_components/DateSelector";
-import ReservationForm from "@/app/_components/ReservationForm";
+import Reservation from "@/app/_components/Reservation";
+import Spinner from "@/app/_components/Spinner";
 import TextExpander from "@/app/_components/TextExpander";
 import {
-  getBookedDatesByCabinId,
   getCabin,
-  getCabins,
-  getSettings,
+  getCabins
 } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
+import { Suspense } from "react";
 
 // export const metadata={title:"Cabin"}
 export async function generateMetadata({ params }) {
@@ -25,15 +24,7 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }) {
-  // const cabin = await getCabin(params.cabinId);
-  // const settings = await getSettings();
-  // const bookedDates = await getBookedDatesByCabinId(params.cabinId);
- const [cabin, settings, bookedDates]= await Promise.all([
-    getCabin(params.cabinId),
-    getSettings(),
-    getBookedDatesByCabinId(params.cabinId),
-  ]);
-
+   const cabin = await getCabin(params.cabinId);
   const { id, name, maxCapacity, regularPrice, discount, image, description } =
     cabin;
 
@@ -87,11 +78,10 @@ export default async function Page({ params }) {
         <h2 className="text-5xl font-semibold text-center mb-10 text-accent-400">
           Reserve {name} today. Pay on arrival.
         </h2>
-
-        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
-          <DateSelector />
-          <ReservationForm />
-        </div>
+        <Suspense fallback={<Spinner/>}>
+        <Reservation cabin={cabin} />
+        </Suspense>
+        
       </div>
     </div>
   );
