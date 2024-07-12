df5d6aebede8a53a8e2e841aa19eb047d16e7fdf
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-12 12:24:49 +0300

    avatar-signin

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -3,7 +3,7 @@ import { auth } from "../_lib/auth";
 
 export default async function Navigation() {
   const session = await auth();
-  console.log(session);
+ // console.log(session);
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -27,9 +27,10 @@ export default async function Navigation() {
           {session?.user?.image ? (
             <Link
               href="/account"
-              className="hover:text-accent-400 transition-colors"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+              
             >
-              <img className="h-8 rounded-full" src= {session.user.image}/>
+              <img className="h-8 rounded-full" src= {session.user.image} alt={session.user.name} referrerPolicy="no-referrer"/>
               <span>Guest area</span>
             </Link>
           ) : (
