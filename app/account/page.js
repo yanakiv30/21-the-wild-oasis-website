c3a4f77286b633c88621269cf702a1087a9d0eb3
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-13 10:20:36 +0300

    title:Login

--- app/account/page.js
+++ app/account/page.js
@@ -1,7 +1,14 @@
-export const metadata={title:"Guest area"};
+import { auth } from "../_lib/auth";
 
-export default function Page() {
-    return <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-    Welcome, Jonas
-  </h2>;
-  }
+export const metadata = { title: "Guest area" };
+
+export default async function Page() {
+  const session = await auth();
+  const firstName = session.user.name.split(" ")[0];
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
+}
