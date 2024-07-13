2d57d7db51dda010a1be99c2d97ac65ac3861084
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-13 11:52:35 +0300

    finish auth

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -24,6 +24,11 @@ const authConfig = {
         return false;
       }
     },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
   },
   pages: {
     signIn: "/login",
