8b0a043534034bbb9ccf0b8d2c1f496cbacf87de
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-14 15:47:28 +0300

    revalidatePath

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,5 +1,6 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 export async function updateGuest(formdata) {
@@ -16,9 +17,11 @@ export async function updateGuest(formdata) {
   const { data, error } = await supabase
     .from("guests")
     .update(updateData)
-    .eq("id", session.user.guestId)    
+    .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath("/account/profile");
 }
 
 export async function signInAction() {
