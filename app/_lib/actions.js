a3df22538c83b0887ee5612e7717cc939dfb6a1b
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-14 23:01:44 +0300

    revalidatePath("/account/reservations"); }

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -24,6 +24,18 @@ export async function updateGuest(formdata) {
   revalidatePath("/account/profile");
 }
 
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
+}
+
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
