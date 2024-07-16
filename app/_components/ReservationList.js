3761c3d04407721a7b3c74435626476e321a6fec
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-16 18:44:55 +0300

    optimisticBookings.map

--- app/_components/ReservationList.js
+++ app/_components/ReservationList.js
@@ -1,13 +1,25 @@
 "use client";
 import ReservationCard from "./ReservationCard";
-import {useOptimistic} from "react";
+import { deleteReservation } from "../_lib/actions";
+
+import { useOptimistic } from "react";
 
 function ReservationList({ bookings }) {
-  useOptimistic();
+  const [optimisticBookings, optimisticDelete] = useOptimistic(
+    bookings,
+    () => {}
+  );
+
+  async function handleDelete(bookingId) {
+    optimisticDelete(bookingId);
+    await deleteReservation(bookingId);
+  }
   return (
     <ul className="space-y-6">
-      {bookings.map((booking) => (
-        <ReservationCard booking={booking} key={booking.id} />
+      {optimisticBookings.map((booking) => (
+        <ReservationCard booking={booking} 
+        onDelete={handleDelete}
+        key={booking.id} />
       ))}
     </ul>
   );
