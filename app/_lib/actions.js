1b2c61cc760e34a2629dcac3005cf3bb539b4920
yanakiv30 <138647535+yanakiv30@users.noreply.github.com>
2024-07-16 13:51:35 +0300

    update and revalidate

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -45,28 +45,33 @@ export async function deleteReservation(bookingId) {
 
 export async function updateBooking(formData) {
   const bookingId = Number(formData.get("bookingId"));
+  //1.Authentication
   const session = await auth();
   if (!session) throw new Error("You must be logged in");
+  //2.Authorization
   const guestBookings = await getBookings(session.user.guestId);
   const guestBookingsIds = guestBookings.map((booking) => booking.id);
   if (!guestBookingsIds.includes(bookingId))
     throw new Error("You are not allowed to update this booking");
-
+  //3.Building update data
   const updateData = {
     numGuests: Number(formData.get("numGuests")),
     observations: formData.get("observations").slice(0, 100),
   };
-
-  
-
+  //4.Mutation
   const { error } = await supabase
     .from("bookings")
     .update(updateData)
     .eq("id", bookingId)
     .select()
     .single();
+  //5.Error handling
   if (error) throw new Error("Booking could not be updated");
-  redirect("/account/reservations")
+  //7.Revalidation
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath("/account/reservations");
+  //7.Redirecting
+  redirect("/account/reservations");
 }
 
 export async function signInAction() {
